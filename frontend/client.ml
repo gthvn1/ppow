c0b4470873ba78b722808a77e20bd3dd54e5fbfe
gthvn1 <gthvn1@gmail.com>
2025-03-01 18:22:40 +0100

    frontend: add canvas

--- frontend/client.ml
+++ frontend/client.ml
@@ -1,12 +1,34 @@
 (* client.ml *)
+open Js_of_ocaml
 
-let () =
-  let open Js_of_ocaml in
-  let onload _ =
-    let doc = Dom_html.document in
-    let h1 = Dom_html.createH1 doc in
-    h1##.textContent := Js.some (Js.string "Hello from OCaml!");
-    Dom.appendChild doc##.body h1;
-    Js._false
-  in
-  Dom_html.window##.onload := Dom_html.handler onload
+let doc = Dom_html.document
+
+let draw_rectangle context =
+  context##fillRect 50. 50. 100. 100. (* Draws a rectangle *)
+
+let create_canvas () =
+  (* Create canvas *)
+  let canvas = Dom_html.createCanvas doc in
+  canvas##.width := 800;
+  canvas##.height := 600;
+  canvas
+
+let create_header (str : string) =
+  let h1 = Dom_html.createH1 doc in
+  h1##.textContent := Js.some (Js.string str);
+  h1
+
+let onload _ =
+  let h1 = create_header "Hello from OCaml" in
+  Dom.appendChild doc##.body h1;
+
+  let canvas = create_canvas () in
+  Graphics_js.open_canvas canvas;
+  Dom.appendChild doc##.body canvas;
+
+  let ctx = canvas##getContext Dom_html._2d_ in
+  draw_rectangle ctx;
+
+  Js._true
+
+let () = Dom_html.window##.onload := Dom_html.handler onload
