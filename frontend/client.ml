c6f4d32112751d8a0e595f8553abfd668874ea1b
Guillaume <guillaume.thouvenin@vates.tech>
2025-03-01 14:34:11 +0100

    frontend: add hello header

--- frontend/client.ml
+++ frontend/client.ml
@@ -1,3 +1,12 @@
 (* client.ml *)
-let () = print_endline "Hello from OCaml"
-(* should print the string in the console *)
+
+let () =
+  let open Js_of_ocaml in
+  let onload _ =
+    let doc = Dom_html.document in
+    let h1 = Dom_html.createH1 doc in
+    h1##.textContent := Js.some (Js.string "Hello from OCaml!");
+    Dom.appendChild doc##.body h1;
+    Js._false
+  in
+  Dom_html.window##.onload := Dom_html.handler onload
